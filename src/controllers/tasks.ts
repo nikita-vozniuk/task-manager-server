646e2ffdc9c03d2f89bc801c4f606922c42ffc1d
nikita-vozniuk <nikitavozniuk0@gmail.com>
2024-04-01 01:21:54 +0300

    Update, Delete Task

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createTask, getUserTasks } from '../db/tasks';
+import { getTaskById, createTask, getUserTasks, updateTaskById, deleteTaskById } from '../db/tasks';
 
 export const createNewTask = async (req: express.Request, res: express.Response) => {
     try {
@@ -39,6 +39,59 @@ export const getAllUserTasks = async (req: express.Request, res: express.Respons
 
         const userTasks = await getUserTasks(userId);
         return res.status(200).json(userTasks).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
+
+export const deleteTask = async (req: express.Request, res: express.Response) => {
+    try {
+        const taskId = req.params.id;
+
+        if (!taskId) {
+            return res.sendStatus(400);
+        }
+
+        const deletedTask = await deleteTaskById(taskId);
+
+        if (!deletedTask) {
+            return res.status(404).json({ message: 'Task not found' }).end();
+        }
+
+        return res.status(200).json(deletedTask).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const updateTask = async (req: express.Request, res: express.Response) => {
+    try {
+        const taskId = req.params.id;
+        const { title, description, dueDate, completed, user } = req.body;
+
+        if (!taskId || (!title && !description && !dueDate && !completed && !user)) {
+            return res.sendStatus(400);
+        }
+
+        const existingTask = await getTaskById(taskId);
+
+        if (!existingTask) {
+            return res.status(404).json({ message: 'Task not found' }).end();
+        }
+
+        const updatedValues = {
+            title: title || existingTask.title,
+            description: description || existingTask.description,
+            dueDate: dueDate || existingTask.dueDate,
+            completed: completed || existingTask.completed,
+            user: user || existingTask.user
+        };
+
+        const updatedTask = await updateTaskById(taskId, updatedValues);
+
+        return res.status(200).json(updatedTask).end();
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
--- src/router/tasks.ts
+++ src/router/tasks.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
-import { createNewTask, getAllUserTasks } from '../controllers/tasks';
+import { createNewTask, getAllUserTasks, updateTask, deleteTask } from '../controllers/tasks';
 
 export default (router: express.Router) => {
     router.post('/tasks', createNewTask);
     router.post('/tasks/user', getAllUserTasks);
+    router.delete('/tasks/:id', deleteTask);
+    router.put('/tasks/:id', updateTask);
 }
